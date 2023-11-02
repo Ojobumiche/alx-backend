ad9918b5ad4b4fe5045e4b19ced45e083dba03cd
Ojobumiche <bumiche@gmail.com>
2023-11-02 09:33:50 +0000

    Basic Flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 """
-0.Basic flask app
+0. Basic Flask app
 """
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"], strict_slash=False)
-def greeting():
-        """
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello() -> str:
+    """
     a route is created with a get method
     """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
