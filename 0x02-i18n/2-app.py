758cc916845bb24cf6e1ab5a5b9d222e35a62bbc
Ojobumiche <bumiche@gmail.com>
2023-11-03 00:01:28 +0000

    2.basic flask app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,21 +3,23 @@
 """
 2. Basic Flask app
 """
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app= Flask(__name__)
+app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config:
     """
-    Config class
+    Config class.
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE ="en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
-    
+
+
 app.config.from_object(Config)
 
 
@@ -27,13 +29,15 @@ def get_locale():
     get_locale.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', methods=["GET"], strict_slashes=False)
-def greetings():
-     """
-     greeting
-     """
-     return render_template('2-index.html')
- 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-    
+def hello():
+    """
+    hello.
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
