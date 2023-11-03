73c94198eaa2ecb95f1b17c7decde02abe5af3ec
Ojobumiche <bumiche@gmail.com>
2023-11-03 04:54:24 +0000

    3-app.py updated

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Flask application """
 
-
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
 
 class Config:
     """ l18n Config class """
@@ -12,19 +12,18 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
-app = Flask(__name__)
-app.config.from_object("1-app.Config")
+app.config.from_object(Config)
 babel = Babel(app)
-
-
-@app.route("/")
-def hello_world():
-    """ Handle default route """
-    return render_template("3-index.html")
-
 
 @babel.localeselector
 def get_locale():
-    """ Gets the best matching language for user """
+    """ Gets the best matching language for the user """
     return request.accept_languages.best_match(Config.LANGUAGES)
+
+@app.route("/")
+def hello_world():
+    """ Handle the default route """
+    return render_template("3-index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
